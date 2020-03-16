b56f80da37bf04e59c9bf1df0aae0a9857fdc39e
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-16 22:14:22 +0900

    Bean 덮어쓰기 방지

--- src/main/java/me/facemoktak/HolomanConfiguration.java
+++ src/main/java/me/facemoktak/HolomanConfiguration.java
@@ -1,5 +1,6 @@
 package me.facemoktak;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class HolomanConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public Holoman holoman() {
         Holoman holoman = new Holoman();
         holoman.setHowLong(35);
